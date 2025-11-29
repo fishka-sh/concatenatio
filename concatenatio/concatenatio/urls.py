6ec15edee75c3b81ece15ffe03d4221a6dfe1bc0
sofya <sofyagrajd@yandex.ru>
2025-11-29 13:12:33 +0500

    мобильная версия

--- concatenatio/concatenatio/urls.py
+++ concatenatio/concatenatio/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('concatenstio_app.urls')),
+    path('', include('concatenatio_app.urls')),
     path('admin/', admin.site.urls),
 ]
