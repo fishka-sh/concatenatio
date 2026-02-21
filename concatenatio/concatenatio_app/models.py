9515c667a99c715740a4ea5364bc212e4a124775
sofya <sofyagrajd@yandex.ru>
2026-02-21 13:03:31 +0500

    смена изображения при наведении в каталоге

--- concatenatio/concatenatio_app/models.py
+++ concatenatio/concatenatio_app/models.py
@@ -1,15 +1,27 @@
 from django.db import models
-
-
-item_types = (('Бр', 'Брелоки'), ('Откр', 'Открытки'), ('Ст', 'Стикеры'), ('Блк', 'Блокноты'), ('Знч', 'Значки'), ('Прч', 'Прочее'))
+from transliterate import translit
 
 class Item(models.Model):
+    item_types = (('Бр', 'Брелоки'),
+                ('Откр', 'Открытки'),
+                ('Ст', 'Стикеры'),
+                ('Блк', 'Блокноты'),
+                ('Знч', 'Значки'),
+                ('Прч', 'Прочее'))
+    
+    
+    def user_directory_path(instance, filename):
+        title = str(translit(value = instance.item_title, language_code = 'ru', reversed = True)).replace('"', '_').replace(' ', '_')
+        id = str(instance.id)
+        return f'item/{id}_{title}/{filename}'
+
     item_title = models.CharField(max_length=100)
     item_price = models.IntegerField()
     item_description = models.TextField()
-    item_image = models.ImageField()
+    item_image = models.ImageField(default='none', upload_to=user_directory_path)
+    item_image_2 = models.ImageField(default='none', upload_to=user_directory_path)
     item_quantity = models.IntegerField()
-    item_types = models.CharField(max_length = 100, choices = item_types)
-       
+    item_types = models.CharField(max_length = 100, choices=item_types)
+    
     def __str__(self):
         return f'{self.item_title}'
