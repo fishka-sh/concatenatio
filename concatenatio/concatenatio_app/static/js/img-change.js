9dad071577abac13b0c6081bebd46afcd7950b4c
EduardSuyargulov <edsuyargulov@gmail.com>
2026-04-02 15:12:33 +0500

    animate fix unsuccesful

--- concatenatio/concatenatio_app/static/js/img-change.js
+++ concatenatio/concatenatio_app/static/js/img-change.js
@@ -1,13 +1,29 @@
 $(document).ready(function(){
+  let time = 500;
   $(".item-img").hover(function() {
     let img = $(this);
     let src = img.attr('src');
-    $(this).attr("src", src.replace('1.jpg', '2.jpg'));
-    $(this).animate(0.25);
-  }, 
-  function(){
+    $(this).attr("src", src.replace('1.jpg', '2.jpg')).css(
+      {
+        'transition-behavior' : 'normal',
+        'transition-duration' : '0.1s',
+        'transition-timing-function' : 'linear',
+        'transition-delay' : '0s',
+        'transition-property' : 'opacity'
+      }
+  )}, 
+  function() {
     let img = $(this);
     let src = img.attr('src');
-    $(this).attr("src", src.replace('2.jpg', '1.jpg'))
-  });
+    $(this).attr("src", src.replace('2.jpg', '1.jpg')).css(
+      {
+        'transition-behavior' : 'normal',
+        'transition-duration' : '0.1s',
+        'transition-timing-function' : 'linear',
+        'transition-delay' : '0s',
+        'transition-property' : 'opacity'
+      }
+  )
+  }
+  );
 });
